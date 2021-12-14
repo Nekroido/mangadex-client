95e85a46cefbe4d286e32f6e12984ac16f63df57
Artem Smirnov <artem.smirnov@mollie.com>
2021-12-14 21:33:57 +0100

    Removed redundant App code

--- src/App/Program.fs
+++ src/App/Program.fs
@@ -1,11 +1,7 @@
-open App
+open Pages
 
 [<EntryPoint>]
 let main _ =
-    let app =
-        application {
-            quality Quality.High
-            language Language.English
-        }
+    Root.initialize ()
 
-    run app
+    0
